06bf40efd2300b33ebb663e147bbfc43357c879d
Shivam Singh <shivam_fw12_029@masai.school>
2022-03-31 18:22:45 +0530

    navbar responsive completed

--- navbar.js
+++ navbar.js
@@ -1,32 +1,38 @@
-function navbar(){
+function navbar() {
     return `<div class="navbar">
     <ul id="menuItems">
-      <li class="active"><a href="index.html">HOME</a></li>
+      <li><a href="index.html">HOME</a></li>
       <li><a href="#">SHOP</a></li>
       <li><a href="#">WHY US</a></li>
       <li><a href="#">TESTIMONIAL</a></li>
-      <li><a href="contact.html">CONTACT US</a></li>
-      <li><a href="account.html">LOGIN</a></li>         
+      <li><a href="contact.html" >CONTACT US</a></li>
+      <li><a href="account.html">LOGIN</a></li> 
+      <li><img src="/images/cart.png" id="cartLogo" alt="cart-image"></li>  
+      
+      <li><input type="text" placeholder="search"></li>  
+
     </ul>
-    <div><img src="/images/cart.png" id="cartLogo" alt="cart-image"></div>
-    <div><input type="text" placeholder="search"></div>
-    <img src="./images/menu.png" class="menu-icon" id="menutoggle">
+    <div> <img src="./images/menu.png" class="menu-icon" id="menutoggle"></div> 
+    
   </div>`
 }
 
 
 
 //-------------menu toggle-------------------//
-function menuToggle() { 
+function menuToggle() {
 
-    if (MenuItems.style.maxHeight === "0px") {
-        MenuItems.style.maxHeight = "200px";
+    if (menuItems.style.maxHeight === "0px") {
+        menuItems.style.maxHeight = "max-content";
+        menuItems.style.display = "block";
     } else {
-        MenuItems.style.maxHeight = "0px";
+        menuItems.style.maxHeight = "0px";
+        menuItems.style.display = "none";
+
     }
 }
 
 
 
 
-export {navbar,menuToggle};
+export { navbar, menuToggle };
