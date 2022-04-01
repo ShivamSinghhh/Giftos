238afc07f744756350d3f68bdf28ca881d3b3834
Shivam Singh <shivam_fw12_029@masai.school>
2022-04-01 11:14:07 +0530

    cartlogo updated

--- navbar.js
+++ navbar.js
@@ -1,5 +1,5 @@
 function navbar() {
-    return `<div class="navbar">
+  return `<div class="navbar">
     <ul id="menuItems">
       <li><a href="index.html">HOME</a></li>
       <li><a href="#">SHOP</a></li>
@@ -7,7 +7,7 @@ function navbar() {
       <li><a href="#">TESTIMONIAL</a></li>
       <li><a href="contact.html" >CONTACT US</a></li>
       <li><a href="account.html">LOGIN</a></li> 
-      <li><img src="/images/cart.png" id="cartLogo" alt="cart-image"></li>  
+      <li><img src="./images/cart.png" id="cartLogo" alt="cart-image"></li>  
       
       <li><input type="text" placeholder="search"></li>  
 
@@ -22,14 +22,14 @@ function navbar() {
 //-------------menu toggle-------------------//
 function menuToggle() {
 
-    if (menuItems.style.maxHeight === "0px") {
-        menuItems.style.maxHeight = "max-content";
-        menuItems.style.display = "block";
-    } else {
-        menuItems.style.maxHeight = "0px";
-        menuItems.style.display = "none";
+  if (menuItems.style.maxHeight === "0px") {
+    menuItems.style.maxHeight = "max-content";
+    menuItems.style.display = "block";
+  } else {
+    menuItems.style.maxHeight = "0px";
+    menuItems.style.display = "none";
 
-    }
+  }
 }
 
 
